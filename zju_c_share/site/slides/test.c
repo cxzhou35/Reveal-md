531679076affbd88494b5613730fc8b31b0af667
cxzhou35 <zjajzcx3566@163.com>
2024-12-12 23:17:04 +0800

    update zju c course sharing

---
+++ zju_c_share/site/slides/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAX 10
+
+int main()
+{
+    int a[MAX] = {10, 2, 5, 3, 6, 8, 1, 4, 9, 7};
+
+    int i, j, temp;
+
+    printf("Before sorting: \n");
+    for (i = 0; i < MAX; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n\n");
+
+    // bubble sort
+    for (i = 0; i < MAX - 1; i++) {
+        for (j = 0; j < MAX - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                // a[j]     = a[j + 1];
+                // temp     = a[j];
+                // a[j + 1] = temp;
+                temp     = a[j];
+                a[j]     = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("After sorting: \n");
+    for (i = 0; i < MAX; i++) {
+        printf("%d ", a[i]);
+    }
+}
